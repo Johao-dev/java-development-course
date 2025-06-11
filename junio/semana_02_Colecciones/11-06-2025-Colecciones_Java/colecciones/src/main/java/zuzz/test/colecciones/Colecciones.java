9295d58aa9868101423fec2bfa4a6fed68d6507f
johaoDev <johaobravoespinoza1501@gmail.com>
2025-06-11 16:25:06 -0500

    clase object

---
+++ junio/semana_02_Colecciones/11-06-2025-Colecciones_Java/colecciones/src/main/java/zuzz/test/colecciones/Colecciones.java
@@ -0,0 +1,12 @@
+package zuzz.test.colecciones;
+
+/**
+ *
+ * @author zuzzDev
+ */
+public class Colecciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Collections!");
+    }
+}
