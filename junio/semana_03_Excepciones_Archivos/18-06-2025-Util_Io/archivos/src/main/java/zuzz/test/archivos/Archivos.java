ac93ee7b9255071e025bf03fbe0d455304da343f
johaoDev <johaobravoespinoza1501@gmail.com>
2025-06-18 16:35:55 -0500

    clase archivos

---
+++ junio/semana_03_Excepciones_Archivos/18-06-2025-Util_Io/archivos/src/main/java/zuzz/test/archivos/Archivos.java
@@ -0,0 +1,12 @@
+package zuzz.test.archivos;
+
+/**
+ *
+ * @author zuzzDev
+ */
+public class Archivos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Files!");
+    }
+}
