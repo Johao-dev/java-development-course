3e5eefd9c90ead5ed7e1ea23465b52c0012eabdf
johaoDev <johaobravoespinoza1501@gmail.com>
2025-06-20 19:06:26 -0500

    clase template method 2

---
+++ junio/semana_03_Excepciones_Archivos/20-06-2025-Mini_Proyecto/template_method/src/main/java/zuzz/test/template_method/Template_method.java
@@ -0,0 +1,12 @@
+package zuzz.test.template_method;
+
+/**
+ *
+ * @author zuzzDev
+ */
+public class Template_method {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Template Method!");
+    }
+}
